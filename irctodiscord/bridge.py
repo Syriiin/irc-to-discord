ab2a919ac0ba1311f05e4cb644362330fcf42f94
Samuel Cattini-Schultz <syriiins@gmail.com>
2019-05-25 14:51:25 +1000

    Consistently use camelCase for config json keys

--- irctodiscord/bridge.py
+++ irctodiscord/bridge.py
@@ -11,15 +11,15 @@ class Bridge(discord.Client):
     def __init__(self, config, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.config = config["discord"]
-        self.channel_pairs = [ChannelPair(pair["irc_channel"], pair["discord_channel"]) for pair in config["pairs"]]
+        self.channel_pairs = [ChannelPair(pair["ircChannel"], pair["discordChannel"]) for pair in config["pairs"]]
         self.irc_client = IRCClient(self, config["irc"], self.channel_pairs)
 
     def run(self):
         self.irc_client_task = self.loop.create_task(self.irc_client.start())
-        super().run(self.config["login_token"])
+        super().run(self.config["loginToken"])
 
     async def on_ready(self):
-        await self.change_presence(activity=discord.Game(name=self.config["status_msg"]))
+        await self.change_presence(activity=discord.Game(name=self.config["statusMessage"]))
     
     async def on_message(self, message):
         if message.author.id not in self.config["ignoreList"] + [self.user.id]:
